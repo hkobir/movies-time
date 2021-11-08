0bbcbb704194fdccf96d32b41709a86f79145470
hkobir <hkobir10@gmail.com>
2021-11-08 21:34:50 +0600

    completed ui of login and home page

--- app/src/main/java/com/hk/movies_time/LoginActivity.java
+++ app/src/main/java/com/hk/movies_time/LoginActivity.java
@@ -1,14 +1,37 @@
 package com.hk.movies_time;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+
+import com.hk.movies_time.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
+    private boolean showPassword = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+        //set pass visibility
+        binding.visibilityIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (showPassword) {
+                    binding.passET.setTransformationMethod(null);
+                    binding.visibilityIV.setImageResource(R.drawable.ic_baseline_visibility_off);
+                    showPassword = false;
+                } else {
+                    binding.passET.setTransformationMethod(new PasswordTransformationMethod());
+                    binding.visibilityIV.setImageResource(R.drawable.ic_baseline_visibility);
+                    showPassword = true;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/hk/movies_time/SplashActivity.java
+++ app/src/main/java/com/hk/movies_time/SplashActivity.java
@@ -1,14 +1,43 @@
 package com.hk.movies_time;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.hk.movies_time.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+
+        //animate
+        Animation animation = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.blink_icon);
+        binding.splashIcon.startAnimation(animation);
+        //post delayed on welcome screen
+        splash();
+    }
+
+    private void splash() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                checkInitialLogic();
+
+            }
+
+
+        }, 2000);
+    }
+
+    private void checkInitialLogic() {
     }
 }
